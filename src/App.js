9bdb4581c00963c970d8b0aaa1f14be4920e0bf5
Meet Amin <76983731+Meetamin001@users.noreply.github.com>
2022-02-09 14:11:02 -0500

    complete_code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
+import Button from '@mui/material/Button';
+import Tooltip from '@material-ui/core/Tooltip';
 
 const App = () => {
   const [val, valChange] = useState(0);
@@ -23,8 +25,15 @@ const App = () => {
           <br />
           <h1>{ val }</h1>
           <br />
-          <button onClick={ incEvent }><AddIcon /></button>
-          <button onClick={ decEvent }><RemoveIcon /></button>
+          <div className="btn_div">
+            <Tooltip title="Add" aria-label="add">
+              <Button onClick={ incEvent } className="btn_green"><AddIcon /></Button>
+            </Tooltip>
+
+            <Tooltip title="Delete" aria-label="delete">
+              <Button onClick={ decEvent } className="btn_red"><RemoveIcon /></Button>
+            </Tooltip>
+          </div>
         </div>
       </div>
     </>
